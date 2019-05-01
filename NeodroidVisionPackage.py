bac905fa399901e01a2833b44e61c6c4f4299444
Christian Heider Nielsen <mrnaah@gmail.com>
2019-05-01 15:46:00 +0200

    Version

--- NeodroidVisionPackage.py
+++ NeodroidVisionPackage.py
@@ -1,6 +1,11 @@
 from setuptools import find_packages
 
-from vision.version import get_version
+import os
+import re
+
+with open(os.path.join(os.path.dirname(__file__), "vision/version.py"), "r") as f:
+    # get version string from module
+    version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M).group(1)
 
 
 class NeodroidVisionPackage:
@@ -148,4 +153,4 @@ class NeodroidVisionPackage:
 
   @property
   def version(self):
-    return get_version()
+    return version
