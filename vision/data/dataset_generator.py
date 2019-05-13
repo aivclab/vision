e13d1e13a2ef254a6952526eff19295a22786834
Christian Heider Nielsen <mrnaah@gmail.com>
2019-05-13 10:36:58 +0200

    Occlusion

--- vision/data/dataset_generator.py
+++ vision/data/dataset_generator.py
@@ -12,43 +12,41 @@ from tqdm import tqdm
 tqdm.monitor_interval = 0
 
 from neodroid.wrappers import CameraObservationWrapper
+from contextlib import suppress
 
 
 def main():
-  _environment = CameraObservationWrapper(connect_to_running=True, image_size=(512, 512, 4))
 
   frame_i = 0
   how_many = 100
-  _environment.reset()
 
-  observation_session = tqdm(_environment, leave=False)
-  for obs in observation_session:
-    frame_i += 1
+  with CameraObservationWrapper(connect_to_running=True) as _environment, suppress(KeyboardInterrupt):
+    observation_session = tqdm(_environment, leave=False)
+    for obs in observation_session:
+      frame_i += 1
 
-    if frame_i % how_many == 0:
-      observation_session.set_description(f'Frame: {frame_i}')
-      break
+      if frame_i % how_many == 0:
+        observation_session.set_description(f'Frame: {frame_i}')
+        break
 
-    rgb = obs['RGB']
-    obj = obs['ObjectSpace']
+      rgb = obs['RGB']
+      obj = obs['ObjectSpace']
 
-    name = f'stepper_{frame_i}'
+      name = f'stepper_{frame_i}'
 
-    imageio.imwrite(name + '.png',rgb)
+      imageio.imwrite(name + '.png',rgb)
 
-    numpy.savez_compressed(name + '.npz', obj.astype(numpy.float16))
+      numpy.savez_compressed(name + '.npz', obj.astype(numpy.float16))
 
-    # with open(name+'.pk', 'wb') as f:
+      # with open(name+'.pk', 'wb') as f:
 
-    # pickle.dump(obs,                  f,                  protocol=pickle.HIGHEST_PROTOCOL)
+      # pickle.dump(obs,                  f,                  protocol=pickle.HIGHEST_PROTOCOL)
 
-    # with open(f'data_{frame_i}.pk', 'rb') as f:
-    #  o = pickle.load(f)
+      # with open(f'data_{frame_i}.pk', 'rb') as f:
+      #  o = pickle.load(f)
 
-    #  print(o == obs)
-    # print(obs)
-
-  _environment.close()
+      #  print(o == obs)
+      # print(obs)
 
 
 if __name__ == '__main__':
