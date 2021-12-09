20f31fbf59b5ef1df4d61b19a21bcbb4b2adb71d
Christian Heider Nielsen <christian.heider@alexandra.dk>
2021-12-09 12:46:50 +0100

    describe intention

--- neodroidvision/multitask/multi_task_learning_scheduler.py
+++ neodroidvision/multitask/multi_task_learning_scheduler.py
@@ -7,23 +7,25 @@ __doc__ = r"""
            Created on 19-09-2021
            """
 
+import torch
 
-def cache_backbone_results():
+
+def cache_backbone_results(model:torch.Module)->None:
     """
-
-    """
-    pass
-
-
-def switch_target_head():
-    """
-
+    Cache and freeze the backbone part of the model and only update heads
     """
     pass
 
 
-def common_head_training():
+def switch_target_head(model:torch.Module)->None:
     """
+    Only single head at a time
+    """
+    pass
 
+
+def common_head_training(model:torch.Module)->None:
+    """
+    Train all heads a the same time
     """
     pass
