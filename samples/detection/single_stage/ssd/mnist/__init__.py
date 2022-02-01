6bf7a4680bcb9efdcd30afc05f039716218fb1eb
Christian Heider Nielsen <christian.heider@alexandra.dk>
2022-02-01 09:47:22 +0100

    Remove sample init files

--- samples/detection/single_stage/ssd/mnist/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__author__ = "{user}"
-__doc__ = r"""
-
-           Created on {date}
-           """
--- samples/eyetest/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__author__ = "heider"
-__doc__ = r"""
-
-           Created on 27/01/2022
-           """
